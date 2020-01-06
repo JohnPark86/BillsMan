fa40fa1066c81e8aef7cfd5529e73b48ccb72c94
John <jduncanpark@gmail.com>
2020-01-05 20:51:52 -0500

    Get something to render on screen!

---
+++ src/js/App.js
@@ -0,0 +1,10 @@
+import React, { useState , useEffect} from 'react';
+
+
+const App = () => {
+    return (
+        <p>The Bills MAnager App will go here</p>
+    );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+/*
+    ./webpack.config.js
+*/
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: path.resolve(__dirname, "src", "index"),
+    output: {
+        filename: 'index.bundle.js',
+        path: path.join(__dirname, "dist")
+      },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Users',
+            template: './src/index.html'
+        })
+    ],
+    devtool: "source-map",
+    devServer: {
+        contentBase: path.join(__dirname, "dist"),
+        compress: true,
+        port: 9000
+    },
+    module: {
+        rules: [
+          { test: /\.js$/, 
+            exclude: /node_modules/, 
+            loader: "babel-loader" },
+          {
+            test: /\.css$/i,
+            use: ['style-loader','css-loader'],
+          }
+        ]
+    }
+};
