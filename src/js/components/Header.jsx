dc7cd6845cf10740920de12a507cbe233f71db69
John <jduncanpark@gmail.com>
2020-05-25 21:07:39 -0400

    Add date to header

--- src/js/components/Header.jsx
+++ src/js/components/Header.jsx
@@ -1,21 +1,38 @@
 import React, { useState, useEffect } from 'react';
+import { format } from 'date-fns';
 
-
-const headerStyle = {
+const HEADER_STYLE = {
 	backgroundColor: '#EEAA7B',
 	borderRadius: '10px',
-	gridTemplateColumns: 'auto 10%'
 };
 
-const titleStyle = {
+const TITLE_CONTAINER = {
+	padding: '0em 2em'
+}
+
+const TITLE_STYLE = {
 	fontFamily: 'Courier New',
-	paddingLeft: '1em'
+	marginBottom: '10px',
+};
+
+const DATE_STYLE = {
+	fontFamily: 'Monaco',
+	fontSize: '18px',
+	letterSpacing: '0.3em',
+	lineHeight: '1',
 };
 
 const Header = (props) => {
+	let today = format(new Date(), "EEEE 'the' do 'of' MMMM");
+
 	return (
-		<div className='grid' style={headerStyle}>
-			<h1 style={titleStyle}>Bills Manager.</h1>
+		<div style={HEADER_STYLE}>
+			<div style={TITLE_CONTAINER}>
+				<h1 style={TITLE_STYLE}>Bills Manager.</h1>
+				<div>
+					<span style={DATE_STYLE}>{today}</span>
+				</div>
+			</div>
 		</div>
 	);
 };
