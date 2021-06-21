b563ff638513d36fb9c0a10051169d267e0dc790
John <jduncanpark@gmail.com>
2021-06-21 11:25:17 -0400

    refactor

--- src/js/App.js
+++ src/js/App.js
@@ -2,32 +2,13 @@ import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import BillsSection from './components/BillsSection';
 import NewBillSection from './components/NewBillSection';
-
+import bills from '../data/bills.json';
 import '../scss/app.scss';
 
 const AppContext = React.createContext();
 
 const App = () => {
-	const [bills, setBills] = useState([
-		{
-			title: 'test1',
-			amount: 500,
-			paid: true,
-			id: 1
-		},
-		{
-			title: 'test2',
-			amount: 250,
-			paid: false,
-			id: 2
-		},
-		{
-			title: 'test3',
-			amount: 300,
-			paid: true,
-			id: 3
-		}
-	]);
+	const [bills, setBills] = useState(bills);
 
 	useEffect(() => {
 		console.log('use effect: ', bills);
